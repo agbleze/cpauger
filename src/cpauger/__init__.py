c4bcf37e3bcaa2958e2eb6f5ca3614440a61a405
linus <lagbleze@st.ug.edu.gh>
2025-08-13 22:00:53 +0200

    feat: add logging setup

--- src/cpauger/__init__.py
+++ src/cpauger/__init__.py
@@ -1,3 +1,18 @@
-# read version from installed package
 from importlib.metadata import version
-__version__ = version("cpauger")
+import logging
+from decouple import config
+from typing import Union
+
+package_name = __name__
+__version__ = version(package_name)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+logging.basicConfig(level=logging.DEBUG, 
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    filename="train_session.logs",
+                    filemode="a"
+                    )
+
+logger.info(f"{package_name} version {__version__} initialized.")
